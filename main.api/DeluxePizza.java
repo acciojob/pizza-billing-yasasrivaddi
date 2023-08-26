cbe3b076ec396cb0aa00437cb8ebe075e4e58d39
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 02:25:49 +0530

    Create DeluxePizza.java

---
+++ main.api/DeluxePizza.java
@@ -0,0 +1,8 @@
+public class DeluxePizza extends Pizza {
+    public DeluxePizza() {
+        super();
+        extraCheese = true;
+        extraToppings = true;
+        basePrice = 400;
+    }
+}
